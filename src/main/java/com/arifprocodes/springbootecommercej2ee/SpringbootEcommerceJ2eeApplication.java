eb26be5eab92126e120d185442489a11802f63e5
arifprocodes <arifulislamcoder@gmail.com>
2024-01-03 23:34:21 -0500

    first commit

---
+++ src/main/java/com/arifprocodes/springbootecommercej2ee/SpringbootEcommerceJ2eeApplication.java
@@ -0,0 +1,13 @@
+package com.arifprocodes.springbootecommercej2ee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcommerceJ2eeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEcommerceJ2eeApplication.class, args);
+	}
+
+}
